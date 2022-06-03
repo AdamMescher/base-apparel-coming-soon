a682f77a21b04e02d3cfd9fb85ec723ca46be09d
Adam Mescher <admescher@gmail.com>
2022-06-02 19:30:06 -0600

    Create theme and css variable design tokens

--- src/components/GlobalStyle/GlobalStyle.ts
+++ src/components/GlobalStyle/GlobalStyle.ts
@@ -1,10 +1,27 @@
 import { createGlobalStyle } from "styled-components";
+import theme from "../../theme";
 
 const GlobalStyle = createGlobalStyle`
+  :root {
+    --desaturated-red: ${theme.color.primary.desaturatedRed};
+    --soft-red: ${theme.color.primary.softRed};
+    --dark-grayish-red: ${theme.color.neutral.darkGrayishRed};
+    --gradient-white-softwhite: ${theme.color.gradient.whiteToSoftWhite};
+    --gradient-softred-pink: ${theme.color.gradient.softRedToPink};
+    --fs-body: ${theme.typography.body.fontSize};
+    --fw-light: ${theme.typography.font.weight.light};
+    --fw-regular: ${theme.typography.font.weight.regular};
+    --fw-semi-bold: ${theme.typography.font.weight.semiBold};
+    --ff-jesefin-sans: ${theme.typography.font.family.josefinSans};
+  }
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+  body {
+    font-size: var(--fs-body);
+    font-family: var(--ff-jesefin-sans);
   }
 `;
 
--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,34 @@
-const theme = {};
+const theme = {
+  color: {
+    primary: {
+      desaturatedRed: "hsl(0, 36%, 70%)",
+      softRed: "hsl(0, 93%, 68%)"
+    },
+    neutral: {
+      darkGrayishRed: "hsl(0, 6%, 24%)"
+    },
+    gradient: {
+      whiteToSoftWhite:
+        "linear-gradient(135deg, hsl(0, 0%, 100%), hsl(0, 100%, 98%))",
+      softRedToPink:
+        "linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))"
+    }
+  },
+  typography: {
+    body: {
+      fontSize: "16px"
+    },
+    font: {
+      family: {
+        josefinSans: "'Josefin Sans', sans-serif"
+      },
+      weight: {
+        light: 300,
+        regular: 400,
+        semiBold: 600
+      }
+    }
+  }
+};
 
 export default theme;
